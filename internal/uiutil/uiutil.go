df6f35a694dd3e36ead0b985b996b45b9a68489d
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-12-15 18:33:24 -0500

    refactor(tui): move UI message handling to internal/uiutil

---
+++ internal/uiutil/uiutil.go
@@ -0,0 +1,58 @@
+// Package uiutil provides utility functions for UI message handling.
+// TODO: Move to internal/ui/<appropriate_location> once the new UI migration
+// is finalized.
+package uiutil
+
+import (
+	"log/slog"
+	"time"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+func CmdHandler(msg tea.Msg) tea.Cmd {
+	return func() tea.Msg {
+		return msg
+	}
+}
+
+func ReportError(err error) tea.Cmd {
+	slog.Error("Error reported", "error", err)
+	return CmdHandler(InfoMsg{
+		Type: InfoTypeError,
+		Msg:  err.Error(),
+	})
+}
+
+type InfoType int
+
+const (
+	InfoTypeInfo InfoType = iota
+	InfoTypeSuccess
+	InfoTypeWarn
+	InfoTypeError
+	InfoTypeUpdate
+)
+
+func ReportInfo(info string) tea.Cmd {
+	return CmdHandler(InfoMsg{
+		Type: InfoTypeInfo,
+		Msg:  info,
+	})
+}
+
+func ReportWarn(warn string) tea.Cmd {
+	return CmdHandler(InfoMsg{
+		Type: InfoTypeWarn,
+		Msg:  warn,
+	})
+}
+
+type (
+	InfoMsg struct {
+		Type InfoType
+		Msg  string
+		TTL  time.Duration
+	}
+	ClearStatusMsg struct{}
+)
